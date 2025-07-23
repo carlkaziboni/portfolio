f4c3fb2fbda8da8726e052ed177d973183d7aa6f
Carl Kaziboni <carlkazibonicman@hotmail.com>
2025-07-23 16:01:48 +0100

    set up database, pages for project applied

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='home_index'),
+    path("fridge/", views.fridge, name='home_fridge'),
+    path("cs50ai/", views.cs50ai, name='home_cs50ai'),
+    path("cs50web/", views.cs50web, name='home_cs50web'),
+    path("humaned/", views.humaned, name='home_humaned'),
+    path("playerposeclassifier/", views.playerposeclassifier, name='home_playerposeclassifier'),
+    path("portfolio/", views.portfolio, name='home_portfolio'),
+    path("suggestions/", views.suggestions, name='home_suggestions'),
+    path("xraydetection/", views.xraydetection, name='home_xraydetection'),
+    path("diageohack/", views.diageohack, name='home_diageohack'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,30 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
+
+def fridge(request):
+    return render(request, 'home/fridge.html')
+
+def cs50ai(request):
+    return render(request, 'home/cs50ai.html')
+
+def cs50web(request):
+    return render(request, 'home/cs50web.html')
+
+def humaned(request):
+    return render(request, 'home/humaned.html')
+
+def playerposeclassifier(request):
+    return render(request, 'home/playerposeclassifier.html')
+
+def portfolio(request):
+    return render(request, 'home/portfolio.html')
+
+def suggestions(request):
+    return render(request, 'home/suggestions.html')
+
+def xraydetection(request):
+    return render(request, 'home/xraydetection.html')
+
+def diageohack(request):
+    return render(request, 'home/diageohack.html')
